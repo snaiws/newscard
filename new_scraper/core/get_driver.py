583dc80a156fd67d4a14a3605118fe7a45a68233
lgh <lghmk2@gmail.com>
2024-11-08 14:24:19 +0900

    Update: headless 테스트

--- new_scraper/core/get_driver.py
+++ new_scraper/core/get_driver.py
@@ -1,9 +1,10 @@
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 def get_driver():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Enable headless mode
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     return driver
