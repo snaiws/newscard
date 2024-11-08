859bcef9bcf7cbcbb07c8b2bbe252c65028dccb7
lgh <lghmk2@gmail.com>
2024-11-08 13:28:17 +0900

    Update: 셀레니움 뉴스 리스트 스크랩

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import streamlit as st
 from streamlit_autorefresh import st_autorefresh
 
 from utils.now import korean_now
-from new_scrapper.selenium import get_news
+from new_scraper.selenium_scraper import get_news
 from data_process.preprocess import process_newscontent
 from summarizer.LLM import openai_api
 from summarizer.prompt_engineering import pm_strategy
@@ -80,7 +80,7 @@ def main():
     for new in news_raw:
         with st.spinner("뉴스를 처리 중입니다..."):
             try:
-                news_contents = new['contents']
+                news_contents = new['content']
                 # 데이터 처리
                 news_processed = process_newscontent(news_contents)
             except:
@@ -122,5 +122,8 @@ def main():
 
 
 if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv()
+
     st_autorefresh(interval=60*1000, key="dataframerefresh")
     main()
---
+++ new_scraper/selenium_scraper.py
@@ -0,0 +1,32 @@
+import os
+import json
+
+from .core.get_driver import get_driver
+from .parser.parser1 import get_headlines_naver_it
+from .parser.parser2 import get_news_naver
+
+
+
+def get_news():
+    # Chrome WebDriver 설정
+    driver = get_driver()
+    target_url = os.getenv("URL_NAVER_NEWS_IT")
+    headlines = get_headlines_naver_it(driver, target_url)
+    news = []
+    for headline in headlines:
+        result = get_news_naver(driver, headline)
+        news.append(result)
+
+    # JSON 파일로 저장
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(news, f, ensure_ascii=False, indent=4)
+
+    driver.quit()
+    print("크롤링 완료, data.json에 저장되었습니다.")
+
+
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv()
+    get_news()
