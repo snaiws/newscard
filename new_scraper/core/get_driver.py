9e746846a419b356162f9ddfdfbb7ff1633fad64
lgh <lghmk2@gmail.com>
2024-11-08 14:50:49 +0900

    Bugfix: headless 옵션추가

--- new_scraper/core/get_driver.py
+++ new_scraper/core/get_driver.py
@@ -5,6 +5,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def get_driver():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Enable headless mode
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")  # 리눅스에서 필수적인 옵션
+    chrome_options.add_argument("--disable-dev-shm-usage")  # 공유 메모리 문제 해결
+    chrome_options.add_argument("--disable-gpu")  # GPU 비활성화 (옵션)
+    chrome_options.add_argument("--window-size=1920,1080")
+
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     return driver
