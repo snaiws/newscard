4dfbf079c95966cc7d07ef91a289c054c4f13673
lgh <lghmk2@gmail.com>
2024-11-08 14:59:16 +0900

    Bugfix: selenium package install test

--- main.py
+++ main.py
@@ -124,8 +124,7 @@ def main():
 
 def install_system_packages():
     try:
-        # 업데이트 및 패키지 설치 명령 실행
-        subprocess.check_call(['apt', 'update'])
+        subprocess.check_call(['apt', 'update', '-o', 'Acquire::ForceIPv4=true'])
         subprocess.check_call(['apt', 'install', '-y', 'google-chrome-stable'])
         subprocess.check_call(['apt', 'install', '-y', 'chromium-driver'])
         print("Packages installed successfully.")
